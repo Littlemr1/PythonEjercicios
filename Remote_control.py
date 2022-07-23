e3751512924d877e28eb604e4216d32bf3f6ff8e
Littlemr1 <47463153+Littlemr1@users.noreply.github.com>
2022-07-23 03:21:05 -0400

    Update and rename control_remoto.py to Remote_control.py

--- Remote_control.py
+++ Remote_control.py
@@ -2,7 +2,7 @@ import pywhatkit
 import flask
 
 # Abrir cmd e ingresar ipconfig para ver tu ip
-# Ingresar en el buscador de tu celular http://ip:8000://control_remoto
+# Ingresar en el buscador de tu celular http://ip:8000://Remote_control
 
 pywhatkit.start_server()
 
