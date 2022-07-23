e99c8b8ad6b504c3aa87c74b2a4fa5199364cbc8
Littlemr1 <47463153+Littlemr1@users.noreply.github.com>
2022-07-23 18:34:25 -0400

    Update getStringUpper.py

--- getStringUpper.py
+++ getStringUpper.py
@@ -1,3 +1,14 @@
+"""
+Question: Define a class which has at least two methods: getString: to get a string from console input printString:
+to print the string in upper case. Also please include simple test function to test the class methods.
+"""
+
+"""
+Pregunta: Defina una clase que tenga al menos dos métodos: getString: para obtener una cadena de la entrada de la consola printString:
+para imprimir la cadena en mayúsculas. También incluya una función de prueba simple para probar los métodos de la clase.
+"""
+
+
 class InputOutString(object):
     def __init__(self):
         self.s = ""
