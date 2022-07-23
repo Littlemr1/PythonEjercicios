c761997e14eadc57a1660c900182edc5857e6550
Littlemr1 <47463153+Littlemr1@users.noreply.github.com>
2022-07-22 20:20:06 -0400

    Add files via upload

---
+++ control_remoto.py
@@ -0,0 +1,8 @@
+import pywhatkit
+import flask
+
+# Abrir cmd e ingresar ipconfig para ver tu ip
+# Ingresar en el buscador de tu celular http://ip:8000://control_remoto
+
+pywhatkit.start_server()
+
