1f4e9e704cedebcc60a87710471c5105695d4cfd
Littlemr1 <47463153+Littlemr1@users.noreply.github.com>
2022-07-23 21:05:53 -0400

    Create integerToString.py

---
+++ integerToString.py
@@ -0,0 +1,12 @@
+"""
+Define a function that can convert a integer into a string and print it in console.
+"""
+
+"""
+Definir una función que pueda convertir un entero en una cadena e imprimirla en consola.
+"""
+
+def printValue(n):
+    print(str(n))
+
+printValue(3)
