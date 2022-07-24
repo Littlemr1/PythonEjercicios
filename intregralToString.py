5e569abe0a07e25e03734f8b5cc182b77b9978f1
Littlemr1 <47463153+Littlemr1@users.noreply.github.com>
2022-07-23 21:07:37 -0400

    Create intregralToString.py

---
+++ intregralToString.py
@@ -0,0 +1,12 @@
+"""
+Define a function that can receive two integral numbers in string form and compute their sum and then print it in console.
+"""
+
+"""
+Definir una función que pueda recibir dos números integrales en forma de cadena y calcular su suma y luego imprimirla en consola.
+"""
+
+def printValue(s1,s2):
+    print(int(s1)+int(s2))
+
+printValue("3","4")
