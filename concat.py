355a0f47051d006e4ec98501cfea3a4e5ed2c044
Littlemr1 <47463153+Littlemr1@users.noreply.github.com>
2022-07-23 21:08:51 -0400

    Create concat.py

---
+++ concat.py
@@ -0,0 +1,12 @@
+"""
+Define a function that can accept two strings as input and concatenate them and then print it in console.
+"""
+
+"""
+Definir una función que pueda aceptar dos cadenas como entrada y concatenarlas y luego imprimirlas en consola.
+"""
+
+def printValue(s1,s2):
+    print(s1+s2)
+
+printValue("3","4") #34
