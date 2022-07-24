3e416566c0b550b2ffb15c14113f7f1ed65526db
Littlemr1 <47463153+Littlemr1@users.noreply.github.com>
2022-07-23 23:03:21 -0400

    Create VerifyEvenList.py

---
+++ VerifyEvenList.py
@@ -0,0 +1,11 @@
+"""
+Please write assert statements to verify that every number in the list [2,4,6,8] is even.
+"""
+
+"""
+Escribe sentencias assert para verificar que cada número de la lista [2,4,6,8] es par.
+"""
+
+li = [2,4,6,8]
+for i in li:
+    assert i%2==0
