0809bdfb9a2c59d0198eea775c891101dde85c48
Littlemr1 <47463153+Littlemr1@users.noreply.github.com>
2022-07-25 17:30:32 -0400

    Create numberValidator.py

---
+++ numberValidator.py
@@ -0,0 +1,14 @@
+def es_numerico(cadena):
+    try:
+        int(cadena)
+        return True
+    except ValueError:
+        return False
+
+
+numero = input('Escriba un número entero: ')
+
+if es_numerico(numero):
+    print('Ha digitado un valor válido.')
+else:
+    print('El valor digitado no corresponde con un número.')
