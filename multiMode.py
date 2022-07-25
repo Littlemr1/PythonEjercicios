077c7dd4f0f6b87c3526e45d52bfb76bd175e5db
Littlemr1 <47463153+Littlemr1@users.noreply.github.com>
2022-07-25 17:45:21 -0400

    Create multiMode.py

---
+++ multiMode.py
@@ -0,0 +1,9 @@
+from statistics import multimode
+
+numeros = [0, 3, 2, 7, 2, 3, 2, 5, 11, 13, 2, 3, 3]
+
+print(multimode(numeros))
+
+lenguajes = ['Java', 'Python', 'C++', 'C++', 'Java', 'Python', 'C', 'JavaScript', 'Java', 'Python', 'Python', 'Java']
+
+print(multimode(lenguajes))
