ecb3bb473b0a1911588fbcf3133b099a42f7b0a2
Littlemr1 <47463153+Littlemr1@users.noreply.github.com>
2022-07-25 17:42:51 -0400

    Create average.py

---
+++ average.py
@@ -0,0 +1,13 @@
+from statistics import mean, fmean
+
+numeros = [2, 3, 5, 7, 11, 13, 17, 19]
+
+promedio = mean(numeros)
+
+print(promedio)
+
+numeros = [2.3, 5.5, 1.11, 17.19, 13.13]
+
+promedio = fmean(numeros)
+
+print(promedio)
