8a684e9ed7f06f099b0076d809f33d58bcedd1e6
Littlemr1 <47463153+Littlemr1@users.noreply.github.com>
2022-07-25 17:35:34 -0400

    Create turtle_Graph.py

---
+++ turtle_Graph.py
@@ -0,0 +1,17 @@
+from turtle import *
+
+color('red', 'yellow')
+
+begin_fill()
+
+while True:
+    forward(400)
+    left(170)
+
+    if abs(pos()) < 1:
+        break
+
+end_fill()
+done()
+Footer
+© 2022 GitHub, Inc.
