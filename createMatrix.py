f314d0666636b9a419e2a717a5243ff5c2145e7e
Littlemr1 <47463153+Littlemr1@users.noreply.github.com>
2022-07-25 18:44:28 -0400

    Create createMatrix.py

---
+++ createMatrix.py
@@ -0,0 +1,13 @@
+def crear_matriz(m, n):
+    """
+    Crea una matriz de m filas y n columnas.
+    """
+    return [[i*j for j in range(n)] for i in range(m)]
+
+
+filas = int(input('Digite la cantidad de filas: '))
+columnas = int(input('Digite la cantidad de columnas: '))
+
+matriz = crear_matriz(filas, columnas)
+
+print(matriz)
