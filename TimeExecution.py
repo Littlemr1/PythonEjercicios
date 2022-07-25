da32ead3ef771efe335e4cb19eb66914230b66eb
Littlemr1 <47463153+Littlemr1@users.noreply.github.com>
2022-07-25 17:33:14 -0400

    Create TimeExecution.py

---
+++ TimeExecution.py
@@ -0,0 +1,21 @@
+from timeit import default_timer
+
+def fibonacci(n):
+    if n == 0 or n == 1:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+inicio = default_timer()
+fibonacci(20)
+fin = default_timer()
+
+print(fin - inicio)
+
+print()
+
+inicio = default_timer()
+fibonacci(37)
+fin = default_timer()
+
+print(fin - inicio)
