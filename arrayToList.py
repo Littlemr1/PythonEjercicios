b0953798c4d4f0cd856b997105d229d1aada17dc
Littlemr1 <47463153+Littlemr1@users.noreply.github.com>
2022-07-25 18:09:28 -0400

    Create arrayToList.py

---
+++ arrayToList.py
@@ -0,0 +1,20 @@
+import ast
+
+lenguajes = "['Python', 'JavaScript', 'C#', 'C++', 'PHP']"
+
+print(type(lenguajes))
+print(isinstance(lenguajes, list))
+print(isinstance(lenguajes, str))
+
+print()
+
+resultado = ast.literal_eval(lenguajes)
+
+print(type(resultado))
+print(isinstance(resultado, list))
+print(isinstance(resultado, str))
+
+print()
+
+print(lenguajes)
+print(resultado)
