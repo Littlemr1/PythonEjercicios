6e38d27c151b593e8b46ef7b0e391f7920af516c
Littlemr1 <47463153+Littlemr1@users.noreply.github.com>
2022-07-25 21:02:54 -0400

    Create isLeap.py

---
+++ isLeap.py
@@ -0,0 +1,16 @@
+def es_bisiesto(agnio):
+    if agnio % 400 == 0:
+        return True
+    elif agnio % 100 == 0:
+        return False
+    elif agnio % 4 == 0:
+        return True
+    else:
+        return False
+
+agnio = 2020
+print('¿El año %i es bisiesto?: %s' % (agnio, es_bisiesto(agnio)))
+agnio = 2013
+print('¿El año %i es bisiesto?: %s' % (agnio, es_bisiesto(agnio)))
+agnio = 2000
+print('¿El año %i es bisiesto?: %s' % (agnio, es_bisiesto(agnio)))
