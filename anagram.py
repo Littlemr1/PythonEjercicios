6be5342b89866020b0fa7c5ceebcb5e2f9abf386
Littlemr1 <47463153+Littlemr1@users.noreply.github.com>
2022-07-29 21:03:06 -0400

    Create anagram.py

---
+++ anagram.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+
+class Solution:
+    def isAnagram(self, s, t):
+      
+        cnt = defaultdict(int)
+        for c in s:
+            cnt[c] += 1
+
+        for c in t:
+            if c not in cnt or cnt[c] < 1:
+                return False
+
+            cnt[c] -= 1
+
+        for v in cnt.values():
+            if v != 0:
+                return False
+
+        return True
