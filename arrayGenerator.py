87128a9c16abbdd6d90d24a2870313af746083b2
Littlemr1 <47463153+Littlemr1@users.noreply.github.com>
2022-07-29 20:43:16 -0400

    Create arrayGenerator.py

---
+++ arrayGenerator.py
@@ -0,0 +1,5 @@
+name = raw_input("\nEnter the name of your array.\n>")
+items = raw_input("\nEnter your strings.\n>")
+array = items.split()
+print "\n\nArray name: ", name
+print name, " = ", array
